f0ec88b33cf27d713575d4fc5599afcec77db1d0
Phucbuaneee08 <92803855+Phucbuaneee08@users.noreply.github.com>
2021-10-19 22:18:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
     titleWeb: "20/10 GỬI EM ",
-    introTitle: 'BÉ CÁ IU CHƯN CỦA ',
+    introTitle: 'BÉ CÁ IU CHƯN CỦA ANH CHÚNNN ',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
+    btnIntro: 'BẤM ĐIII',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    btnYes: 'IU NHẮMMM',
+    btnNo: 'KHÔNG IU, HỨ',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao cá lại yêu anh chún <3',
+    btnReply: 'Gửi cho anh chún iuuu <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    mess: 'Anh biết mà 🥰. Yêu em nhìu nhắm nhiều nhiều 😘😘',
+    messDesc: 'MÃI BÊN ANH , IU ANH NHA CÁ IU XINH ĐẸP CỦA ANHHHH <3 IU EM NHÌU NHẮMMM ÍIIII',
+    btnAccept: 'Dạaaaaa',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
