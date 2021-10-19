d0bebecf4810d5cf6806b94f9324e5f755ff9e9a
Phucbuaneee08 <92803855+Phucbuaneee08@users.noreply.github.com>
2021-10-19 22:10:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "20/10 GỬI EM ",
+    introTitle: 'BÉ CÁ IU CHƯN CỦA ',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
